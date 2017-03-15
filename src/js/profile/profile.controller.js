d8b15cc6100093f9864f9d004bfb0b5a200b3663
Dan Calise <calise@gmail.com>
2017-03-15 15:49:26 -0400

    updating profile controller so you can save your name

--- src/js/profile/profile.controller.js
+++ src/js/profile/profile.controller.js
@@ -1,9 +1,24 @@
 class ProfileCtrl {
-  constructor() {
+  constructor(md5, auth, profile, Users) {
     'ngInject';
 
+    this._md5 = md5;
+    this._auth = auth;
+    this._profile = profile;
+    this._Users = Users;
+
+  }
+
+  updateProfile() {
+    this._profile.emailHash = this._md5.createHash(this._auth.email)
+    this._profile.$save().then(
+      // () => console.log(this._profile)
+    )
+  }
+
+  signOut() {
+    this._Users.signOut()
   }
 }
-
 
 export default ProfileCtrl;
