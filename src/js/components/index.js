bfe44da3567d3d3149ea14d85365f3d9919a6dcf
Dan Calise <calise@gmail.com>
2017-03-15 21:04:59 -0400

    showauthed.. disabled temporarily

--- src/js/components/index.js
+++ src/js/components/index.js
@@ -2,6 +2,9 @@ import angular from 'angular';
 
 let componentsModule = angular.module('app.components', []);
 
+// import ShowAuthed from './show-authed.directive';
+// componentsModule.directive('showAuthed', ShowAuthed);
+
 import CustomerList from './request-helpers/customer-list.component';
 componentsModule.component('customerList', CustomerList);
 
