193fc2837e7fc59f99d320f91720ce50949cb1c4
Dan Calise <calise@gmail.com>
2017-04-01 13:09:09 -0400

    adding account service

---
+++ src/js/services/accounts.service.js
@@ -0,0 +1,21 @@
+export default class Accounts {
+  constructor($firebaseArray, $firebaseObject) {
+    'ngInject';
+
+    this.accountsRef = firebase.database().ref('accounts');
+    this.accounts = $firebaseArray(this.accountsRef);
+
+    this.all = this.accounts;
+  }
+
+  add(account) {
+    name: account.name,
+    accountType: account.accountType,
+    cid: account.cid,
+    country: account.country,
+    value: account.value,
+    dateCreated: Date.now(),
+    lastUpdated: null,
+    editedBy: null
+  }
+}
