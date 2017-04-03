bdd12f03a5f5aae8da1626c24cf7777a4d9e065b
Dan Calise <calise@gmail.com>
2017-04-03 18:06:15 -0400

    attaching account meta to features

--- src/js/features/features.controller.js
+++ src/js/features/features.controller.js
@@ -6,6 +6,17 @@ class FeaturesCtrl {
     this._Accounts = Accounts;
 
     this.features = this._Features.all
+
+    this.features.$loaded().then(function(features){
+      angular.forEach(features, function(feature) {
+
+        feature.accountsMeta = [];
+        Accounts.getAccount(feature.accounts[0]).then(
+          (account) => feature.accountsMeta.push(account)
+        )
+        
+      })
+    });
     
   }
   
