ad099f46673ea632e30d086640a8d1c2fdb17eaf
Dan Calise <dan.calise@prnewswire.com>
2017-04-03 22:22:22 -0400

    getting feature and building accounts meta

--- src/js/feature/feature.controller.js
+++ src/js/feature/feature.controller.js
@@ -1,10 +1,24 @@
 class FeatureCtrl {
-  constructor(Features, $http, $q, $stateParams) {
+  constructor(Features, Accounts, $stateParams) {
     'ngInject';
 
     Features.getFeature($stateParams.id).then(
-      (feature) => this.feature = feature
-    );
+      (feature) => {
+
+        feature.accountsMeta = [];
+        feature.totalValue = 0;
+        angular.forEach(feature.accounts, (account) => {
+          Accounts.getAccount(account).then(
+            (account) => {
+              feature.accountsMeta.push(account)
+              feature.totalValue += account.value
+            }
+          )
+        })
+        this.feature = feature
+
+      }
+    )
 
   }
 }
