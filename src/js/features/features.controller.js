1c331fe478401e0dc9ef33f8a513300187d45f36
Dan Calise <dan.calise@prnewswire.com>
2017-04-03 22:22:33 -0400

    refactor

--- src/js/features/features.controller.js
+++ src/js/features/features.controller.js
@@ -3,28 +3,27 @@ class FeaturesCtrl {
     'ngInject';
 
     this._Features = Features;
-    this._Accounts = Accounts;
 
     this.features = this._Features.all
 
-    this.features.$loaded().then(function(features){
-      angular.forEach(features, function(feature) {
+    this.features.$loaded().then(
+      (features) => {
+        angular.forEach(features, function(feature) {
 
-        feature.accountsMeta = [];
-        feature.totalValue = 0;
-        angular.forEach(feature.accounts, function(account) {
-          Accounts.getAccount(account).then(
-            (account) => {
-              feature.accountsMeta.push(account)
-              feature.totalValue += account.value
-            }
-          )
+          feature.accountsMeta = [];
+          feature.totalValue = 0;
+          angular.forEach(feature.accounts, function(account) {
+            Accounts.getAccount(account).then(
+              (account) => {
+                feature.accountsMeta.push(account)
+                feature.totalValue += account.value
+              }
+            )
+          })
+          
         })
-
-
-      })
-    });
-    
+      }
+    )
   }
   
 }
