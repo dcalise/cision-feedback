40d5c1ddeb600f6f3a2b6d2001274ee8874740fc
Dan Calise <dan.calise@prnewswire.com>
2017-04-06 00:01:31 -0400

    grabbing feature from resolve

--- src/js/feature/feature.controller.js
+++ src/js/feature/feature.controller.js
@@ -1,36 +1,51 @@
 class FeatureCtrl {
-  constructor(Features, Accounts, $stateParams, Users) {
+  constructor(feature, Features, Accounts, $stateParams, Users) {
     'ngInject';
 
+    this._$stateParams = $stateParams;
+
+    this._feature = feature;
     this._Accounts = Accounts;
     this._Features = Features;
 
-    Features.getFeature($stateParams.id).then(
-      (feature) => {
+    this._featureDetail = {
+      accountsMeta: [],
+      totalValue: 0
+    }
 
-        feature.accountsMeta = [];
-        feature.totalValue = 0;
-        angular.forEach(feature.accounts, (account) => {
-          Accounts.getAccount(account).then(
-            (account) => {
-              feature.accountsMeta.push(account)
-              feature.totalValue += parseInt(account.value)
-            }
-          )
-        })
-        feature.requester = Users.getProfile(feature.requesterUID);
-        this.feature = feature
+    angular.forEach(this._feature.accounts, (account) => {
+      Accounts.getAccount(account).then(
+        (account) => {
+          this._featureDetail.accountsMeta.push(account)
+          this._featureDetail.totalValue += parseInt(account.value)
+        }
+      )
+    })
+    this._featureDetail.requester = Users.getProfile(feature.requesterUID);
 
-      }
-    )
+    console.log(this._feature.accounts)
+    console.log(this._featureDetail)
 
   }
 
   addAccount() {
     this._Accounts.add(this.accountForm).then(
       (account) => {
-        this.feature.accounts.push(account.key)
-        console.log(this.feature)
+        this._feature.accounts.push(account)
+        // this._Features.addAccountToFeature(account.key, this._feature.$id).then(
+        //   (feature) => {
+        //     this.feature = feature
+        //     this.feature.$save()
+        //   }
+        // )
+        this._feature.$save().then(
+          () => console.log(this._feature)
+        )
+        // this.feature.$save().then(
+        //   () => console.log(this.feature)
+        // )
+
+
       },
       (error) => { console.log(error) }
     )
