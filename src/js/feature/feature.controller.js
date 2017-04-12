48c6194d093eeafa2fc5648b92da13922a699bc2
Dan Calise <calise@gmail.com>
2017-04-11 21:11:59 -0400

    updating on change!

--- src/js/feature/feature.controller.js
+++ src/js/feature/feature.controller.js
@@ -7,25 +7,23 @@ class FeatureCtrl {
     this._feature = feature;
     this._Accounts = Accounts;
     this._Features = Features;
+    this._Users = Users;
 
-    this._featureDetail = {
-      accountsMeta: [],
-      totalValue: 0
-    }
+    this._featureDetail = {}
 
+  }
+  listAccounts() {
+    this._featureDetail.accountsMeta = []
+    this._featureDetail.totalValue = 0
     angular.forEach(this._feature.accounts, (account) => {
-      Accounts.getAccount(account).then(
+      this._Accounts.getAccount(account).then(
         (account) => {
           this._featureDetail.accountsMeta.push(account)
           this._featureDetail.totalValue += parseInt(account.value)
         }
       )
     })
-    this._featureDetail.requester = Users.getProfile(feature.requesterUID);
-
-    console.log(this._feature.accounts)
-    console.log(this._featureDetail)
-
+    this._featureDetail.requester = this._Users.getProfile(this._feature.requesterUID);
   }
 
   addAccount() {
@@ -34,9 +32,8 @@ class FeatureCtrl {
         console.log(account.key)
         this._feature.accounts.push(account.key)
 
-
         return this._feature.$save().then(
-          () => console.log(this._feature),
+          () => this.listAccounts(),
           (error) => console.log(error)
         )
 
