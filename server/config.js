f0fbe5dca7309db9c7fad2d8eb9d9c7fd35781ea
Dan Calise <dan.calise@prnewswire.com>
2017-05-04 13:40:16 -0400

    removing server files

--- server/config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-
-    'secret': 'ilovescotchyscotch',
-    'database': 'mongodb://cision:c1s10n@olympia.modulusmongo.net:27017/P8upihiw'
-
-};
--- server/services/db.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const autoIncrement = require('mongoose-auto-increment');
-const config = require('./../config');
-
-const connection = mongoose.connect(config.database);
-
-autoIncrement.initialize(connection);
-
-module.exports = connection;
