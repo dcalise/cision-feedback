08096d2963627712e644ad9b09ac9ec991e2f69f
Dan Calise <dan.calise@prnewswire.com>
2017-08-08 08:13:35 -0400

    adding display-label component where applicable

--- src/app/components/display-label/display-label.component.js
+++ src/app/components/display-label/display-label.component.js
@@ -6,20 +6,38 @@ class DisplayLabelCtrl {
     this.displayName;
   }
 
-  getDisplayName(labelId) {
-    this._LabelService.getLabel(labelId).$loaded().then(
-      (label) => {
-        this.displayName = label.displayName
-        console.log(this.displayName)
-      }
-    )
+  getDisplayName(labelId, labelType) {
+    switch (labelType) {
+      case 'product':
+        this._LabelService.getProduct(labelId).$loaded().then(
+          (product) => {
+            this.displayName = product.displayName
+          }
+        )
+        break;
+      case 'location':
+        console.log('location');
+        this._LabelService.getLocation(labelId).$loaded().then(
+          (location) => {
+            this.displayName = location.displayName
+          }
+        )
+        break;
+      default:
+        this._LabelService.getLabel(labelId).$loaded().then(
+          (label) => {
+            this.displayName = label.displayName
+          }
+        )
+    }
   }
 }
 
 let DisplayLabel = {
   bindings: {
     data: '=',
-    lineBreak: '='
+    lineBreak: '=',
+    labelType: '='
   },
   controller: DisplayLabelCtrl,
   templateUrl: 'components/display-label/display-label.html'
