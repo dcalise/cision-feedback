f264cb942c9aa62171348cd99842cf4736b5feec
Dan Calise <dan.calise@prnewswire.com>
2017-11-28 17:44:59 -0500

    switching back to single quotes

--- src/app/auth/auth.config.js
+++ src/app/auth/auth.config.js
@@ -3,63 +3,63 @@ function AuthConfig($stateProvider, $httpProvider) {
 
   $stateProvider
 
-    .state("app.login", {
-      url: "/login",
-      controller: "AuthCtrl as $ctrl",
-      templateUrl: "auth/login.html",
-      title: "Sign in",
+    .state('app.login', {
+      url: '/login',
+      controller: 'AuthCtrl as $ctrl',
+      templateUrl: 'auth/login.html',
+      title: 'Sign in',
       resolve: {
         requireNoAuth: function($state, AuthService) {
           return AuthService.$requireSignIn().then(
             auth => {
-              $state.go("app.feature-list");
+              $state.go('app.feature-list')
             },
             err => {
-              return;
+              return
+            }
+          )
+        }
+      }
+    })
+
+    .state('app.register', {
+      url: '/register',
+      controller: 'AuthCtrl as $ctrl',
+      templateUrl: 'auth/register.html',
+      title: 'Sign up',
+      resolve: {
+        requireNoAuth: function($state, AuthService) {
+          return AuthService.$requireSignIn().then(
+            auth => {
+              $state.go('app.feature-list')
+            },
+            err => {
+              return
+            }
+          )
+        }
+      }
+    })
+
+    .state('app.forgot-password', {
+      url: '/forgot-password',
+      controller: 'AuthCtrl as $ctrl',
+      templateUrl: 'auth/forgot-password.html',
+      title: 'Forgot Password',
+      resolve: {
+        requireNoAuth: function($state, AuthService) {
+          return AuthService.$requireSignIn().then(
+            auth => {
+              $state.go('app.profile')
+            },
+            err => {
+              return
             }
           );
         }
       }
     })
-
-    .state("app.register", {
-      url: "/register",
-      controller: "AuthCtrl as $ctrl",
-      templateUrl: "auth/register.html",
-      title: "Sign up",
-      resolve: {
-        requireNoAuth: function($state, AuthService) {
-          return AuthService.$requireSignIn().then(
-            auth => {
-              $state.go("app.feature-list");
-            },
-            err => {
-              return;
-            }
-          );
-        }
-      }
-    })
-
-    .state("app.forgot-password", {
-      url: "/forgot-password",
-      controller: "AuthCtrl as $ctrl",
-      templateUrl: "auth/forgot-password.html",
-      title: "Forgot Password",
-      resolve: {
-        requireNoAuth: function($state, AuthService) {
-          return AuthService.$requireSignIn().then(
-            auth => {
-              $state.go("app.profile");
-            },
-            err => {
-              return;
-            }
-          );
-        }
-      }
-    });
 
 }
 
-export default AuthConfig;
+export default AuthConfig
