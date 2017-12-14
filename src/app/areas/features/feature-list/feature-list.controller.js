19829cbcc7f10ebbb118f645b05a7feec1f1f5d8
Dan Calise <dan.calise@prnewswire.com>
2017-12-14 16:04:53 -0500

    adding reset button, reset function

--- src/app/areas/features/feature-list/feature-list.controller.js
+++ src/app/areas/features/feature-list/feature-list.controller.js
@@ -1,109 +1,111 @@
 class FeatureListCtrl {
-    constructor($http, $q, $localStorage, FeatureService, AccountService) {
-        'ngInject';
+  constructor($http, $q, $localStorage, FeatureService, AccountService) {
+    "ngInject";
 
-        this._FeatureService = FeatureService;
-        this.$localStorage = $localStorage;
+    this._FeatureService = FeatureService;
+    this.$localStorage = $localStorage;
 
-        this.features = this._FeatureService.all;
+    this.features = this._FeatureService.all;
 
-        this.features.$loaded().then(features => {
-            angular.forEach(features, function(feature) {
-                feature.accountsMeta = [];
-                feature.totalValue = 0;
-                angular.forEach(feature.accounts, function(account) {
-                    AccountService.getAccount(account.accountKey).then(
-                        account => {
-                            feature.accountsMeta.push(account);
-                            feature.totalValue += parseInt(account.value);
-                        }
-                    );
-                });
-            });
+    this.features.$loaded().then(features => {
+      angular.forEach(features, function(feature) {
+        feature.accountsMeta = [];
+        feature.totalValue = 0;
+        angular.forEach(feature.accounts, function(account) {
+          AccountService.getAccount(account.accountKey).then(account => {
+            feature.accountsMeta.push(account);
+            feature.totalValue += parseInt(account.value);
+          });
         });
+      });
+    });
+  }
+
+  $onInit() {
+    this.searchFeatures = "";
+
+    // set default
+    if (typeof this.getTablePrefs() === "undefined") {
+      this.resetTablePrefs();
+    } else {
+      this.tablePrefs = {
+        type: this.getTablePrefs().type,
+        reverse: this.getTablePrefs().reverse,
+        width: this.getTablePrefs().width,
+        columns: this.getTablePrefs().columns
+      };
     }
+  }
 
-    $onInit() {
-        this.searchFeatures = '';
+  changeColumnSort(col, reverse) {
+    this.tablePrefs.reverse = false;
+    if (this.tablePrefs.type == col) {
+      this.tablePrefs.reverse = !reverse;
+    } else {
+      this.tablePrefs.reverse = false;
+      this.tablePrefs.type = col;
+    }
+    this.setTablePrefs(this.tablePrefs);
+  }
 
-        // set default
-        if (typeof this.getTablePrefs() === 'undefined') {
-            this.tablePrefs = {
-                type: 'dateCreated',
-                reverse: true,
-                width: 'container',
-                columns: {
-                    status: {
-                        id: 'status',
-                        display: true,
-                        displayName: 'Status'
-                    },
-                    labels: {
-                        id: 'labels',
-                        display: true,
-                        displayName: 'Labels'
-                    },
-                    summary: {
-                        id: 'subject',
-                        display: true,
-                        displayName: 'Summary'
-                    },
-                    originalRequester: {
-                        id: 'accountsMeta[0].name',
-                        display: true,
-                        displayName: 'Original Requester'
-                    },
-                    date: {
-                        id: 'dateCreated',
-                        display: true,
-                        displayName: 'Date'
-                    },
-                    totalValue: {
-                        id: 'totalValue',
-                        display: true,
-                        displayName: 'Total Value',
-                        thAlign: 'text-right'
-                    }
-                }
-            };
-            // grab from local storage
-        } else {
-            this.tablePrefs = {
-                type: this.getTablePrefs().type,
-                reverse: this.getTablePrefs().reverse,
-                width: this.getTablePrefs().width,
-                columns: this.getTablePrefs().columns
-            };
+  changeTableWidth(width) {
+    this.tablePrefs.width = width;
+    this.setTablePrefs(this.tablePrefs);
+  }
+
+  changeColumnVisibility(column, key) {
+    this.setTablePrefs(this.tablePrefs);
+  }
+
+  setTablePrefs(prefs) {
+    this.$localStorage.tablePrefsSaved = prefs;
+  }
+
+  getTablePrefs() {
+    return this.$localStorage.tablePrefsSaved;
+  }
+
+  resetTablePrefs() {
+    this.tablePrefs = {
+      type: "dateCreated",
+      reverse: true,
+      width: "container",
+      columns: {
+        status: {
+          id: "status",
+          display: true,
+          displayName: "Status"
+        },
+        labels: {
+          id: "labels",
+          display: true,
+          displayName: "Labels"
+        },
+        summary: {
+          id: "subject",
+          display: true,
+          displayName: "Summary"
+        },
+        originalRequester: {
+          id: "accountsMeta[0].name",
+          display: true,
+          displayName: "Original Requester"
+        },
+        date: {
+          id: "dateCreated",
+          display: true,
+          displayName: "Date"
+        },
+        totalValue: {
+          id: "totalValue",
+          display: true,
+          displayName: "Total Value",
+          thAlign: "text-right"
         }
-    }
-
-    changeColumnSort(col, reverse) {
-        this.tablePrefs.reverse = false;
-        if (this.tablePrefs.type == col) {
-            this.tablePrefs.reverse = !reverse;
-        } else {
-            this.tablePrefs.reverse = false;
-            this.tablePrefs.type = col;
-        }
-        this.setTablePrefs(this.tablePrefs);
-    }
-
-    changeTableWidth(width) {
-        this.tablePrefs.width = width;
-        this.setTablePrefs(this.tablePrefs);
-    }
-
-    changeColumnVisibility(column, key) {
-        this.setTablePrefs(this.tablePrefs);
-    }
-
-    setTablePrefs(prefs) {
-        this.$localStorage.tablePrefsSaved = prefs;
-    }
-
-    getTablePrefs() {
-        return this.$localStorage.tablePrefsSaved;
-    }
+      }
+    };
+    this.setTablePrefs(this.tablePrefs);
+  }
 }
 
 export default FeatureListCtrl;
