5a8018171e3babae19baeb3b121a7d48ab404799
Dan Calise <dan.calise@prnewswire.com>
2018-01-09 15:00:08 -0500

    starting email verificaiton send

--- src/app/auth/auth.controller.js
+++ src/app/auth/auth.controller.js
@@ -1,61 +1,93 @@
 class AuthCtrl {
-  constructor(AuthService, $state) {
-    'ngInject'
+    constructor(AuthService, $state) {
+        'ngInject';
 
-    this._$state = $state
-    this._AuthService = AuthService
-   
-    this.error = null
-  }
+        this._$state = $state;
+        this._AuthService = AuthService;
 
-  submitLogin() {
-    this.isSubmitting = true
-    this._AuthService.$signInWithEmailAndPassword(this.formData.email, this.formData.password).then(
-      (res) => {
-        this.isSubmitting = false
-        this._$state.go('app.home')
-      },
-      (err) => {
-        console.log(err)
-        this.isSubmitting = false
-        this.error = err
-      }
-    )
-  }
-  submitRegister() {
-    let domain = this.formData.email.split('@')[1];
-    if (domain === 'prnewswire.com' || domain === 'cision.com' || domain === 'prnewswire.co.uk' || domain === 'multivu.com' || domain === 'gorkana.com' || domain === 'newswire.ca') {
-      this.isSubmitting = true
-      this._AuthService.$createUserWithEmailAndPassword(this.formData.email, this.formData.password).then(
-        (res) => {
-          this.isSubmitting = false
-          this._$state.go('app.profile')
-        },
-        (err) => {
-          this.isSubmitting = false
-          this.error = err
-        }
-      )
-    } else {
-      this.error.message = "Sorry, you can't register with that email address. Please contact the administrator for for information."
+        this.error = null;
     }
-  }
 
-  resetPassword() {
-    this._AuthService.$sendPasswordResetEmail(this.formData.email).then(
-      () => {
-        let email = this.formData.email
-        this.formData.email = ''
-        this.error = null
-        this.info = `A password reset email has been sent to ${ email }.`
-      },
-      (err) => {
-        this.error = err
-        this.info = null
-      }
-    )
-  }
+    sendFirebaseEmailVerification() {
+        let user = firebase.auth().currentUser;
+        user
+            .sendEmailVerification()
+            .then(success => {
+                console.log('please verify your email');
+            })
+            .catch(err => {
+                this.error = err;
+            });
+
+        console.log('sendddd');
+    }
+
+    submitLogin() {
+        this.isSubmitting = true;
+        this._AuthService
+            .$signInWithEmailAndPassword(
+                this.formData.email,
+                this.formData.password
+            )
+            .then(
+                res => {
+                    this.isSubmitting = false;
+                    this._$state.go('app.home');
+                    this.sendFirebaseEmailVerification();
+                },
+                err => {
+                    console.log(err);
+                    this.isSubmitting = false;
+                    this.error = err;
+                }
+            );
+    }
+    submitRegister() {
+        let domain = this.formData.email.split('@')[1];
+        if (
+            domain === 'prnewswire.com' ||
+            domain === 'cision.com' ||
+            domain === 'prnewswire.co.uk' ||
+            domain === 'multivu.com' ||
+            domain === 'gorkana.com' ||
+            domain === 'newswire.ca'
+        ) {
+            this.isSubmitting = true;
+            this._AuthService
+                .$createUserWithEmailAndPassword(
+                    this.formData.email,
+                    this.formData.password
+                )
+                .then(
+                    res => {
+                        this.isSubmitting = false;
+                        this._$state.go('app.profile');
+                    },
+                    err => {
+                        this.isSubmitting = false;
+                        this.error = err;
+                    }
+                );
+        } else {
+            this.error.message =
+                "Sorry, you can't register with that email address. Please contact the administrator for for information.";
+        }
+    }
+
+    resetPassword() {
+        this._AuthService.$sendPasswordResetEmail(this.formData.email).then(
+            () => {
+                let email = this.formData.email;
+                this.formData.email = '';
+                this.error = null;
+                this.info = `A password reset email has been sent to ${email}.`;
+            },
+            err => {
+                this.error = err;
+                this.info = null;
+            }
+        );
+    }
 }
 
-
-export default AuthCtrl
+export default AuthCtrl;
