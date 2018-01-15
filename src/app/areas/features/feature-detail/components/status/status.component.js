ff5217d957d5f1cec50d355918a19aa02760e38e
Dan Calise <dan.calise@prnewswire.com>
2018-01-15 17:08:47 -0500

    updating formatting

--- src/app/areas/features/feature-detail/components/status/status.component.js
+++ src/app/areas/features/feature-detail/components/status/status.component.js
@@ -1,33 +1,32 @@
 class StatusCtrl {
-  constructor(AuthService, UserService) {
-    'ngInject';
+    constructor(AuthService, UserService) {
+        'ngInject';
 
-    let initStatus;
-    this.changeStatus = function(data) {
-      if (this.editing) {
-        if (data.status === 'cancel') {
-          this.data = initStatus
-        } else {
-          this.updateStatus(status)
-        }
-        this.editing = false
-      } else {
-        initStatus = this.data
-        this.editing = true
-      }
+        let initStatus;
+        this.changeStatus = function(data) {
+            if (this.editing) {
+                if (data.status === 'cancel') {
+                    this.data = initStatus;
+                } else {
+                    this.updateStatus(status);
+                }
+                this.editing = false;
+            } else {
+                initStatus = this.data;
+                this.editing = true;
+            }
+        };
     }
-
-  }
 }
 
 let Status = {
-  bindings: {
-    admin: '<',
-    data: '=',
-    updateStatus: '&'
-  },
-  controller: StatusCtrl,
-  templateUrl: 'areas/features/feature-detail/components/status/status.html'
+    bindings: {
+        admin: '<',
+        data: '=',
+        updateStatus: '&'
+    },
+    controller: StatusCtrl,
+    templateUrl: 'areas/features/feature-detail/components/status/status.html'
 };
 
 export default Status;
