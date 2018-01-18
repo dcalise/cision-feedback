058c8eb0f97609e78d91e18601f6ebc7208c8f7a
Dan Calise <dan.calise@prnewswire.com>
2018-01-18 11:56:50 -0500

    fixing auth bug created by email sign in

--- src/app/areas/admin/admin.config.js
+++ src/app/areas/admin/admin.config.js
@@ -13,6 +13,7 @@ function AdminConfig($stateProvider) {
                     if (!auth.emailVerified) {
                         $state.go('app.profile');
                     }
+                    return auth;
                 });
             },
             profile: function(UserService, AuthService) {
