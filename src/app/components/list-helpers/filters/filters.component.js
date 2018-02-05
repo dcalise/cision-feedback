7dd9a81840d5af197db368c2665bcdd176aa3a7f
Dan Calise <dan.calise@prnewswire.com>
2018-02-05 12:24:40 -0500

    filtering for status

--- src/app/components/list-helpers/filters/filters.component.js
+++ src/app/components/list-helpers/filters/filters.component.js
@@ -1,28 +1,27 @@
 class FiltersCtrl {
-    constructor(FilteredArrayFactory) {
+    constructor() {
         'ngInject';
     }
 
     $onInit() {
+        this.filterParams = {};
 
-        this.filterParams = {}
-      
         this.statusList = ['Received', 'Viewed', 'Closed'];
-        this.filterParams.status = ['Received', 'Viewed', 'Closed'];
+        this.filterParams.status = this.statusList.slice(0);
+
+        this.updateFilters({ filterParams: this.filterParams });
     }
 
     toggleSelection(status) {
+        let idx = this.filterParams.status.indexOf(status);
 
-      let idx = this.filterParams.status.indexOf(status);
+        if (idx > -1) {
+            this.filterParams.status.splice(idx, 1);
+        } else {
+            this.filterParams.status.push(status);
+        }
 
-      if (idx > -1) {
-        this.filterParams.status.splice(idx, 1);
-      } else {
-        this.filterParams.status.push(status);
-      }
-
-      this.updateFilters({filterParams: this.filterParams});
-
+        this.updateFilters({ filterParams: this.filterParams });
     }
 }
 
