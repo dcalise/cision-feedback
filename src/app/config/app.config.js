2aafc2eaf269e267312d5166e56134a957be1403
Dan Calise <dan.calise@prnewswire.com>
2018-02-06 10:29:50 -0500

    formatting

--- src/app/config/app.config.js
+++ src/app/config/app.config.js
@@ -1,36 +1,44 @@
-function AppConfig($httpProvider, $stateProvider, $locationProvider, $urlRouterProvider, toastrConfig) {
-  'ngInject';
+function AppConfig(
+    $httpProvider,
+    $stateProvider,
+    $locationProvider,
+    $urlRouterProvider,
+    toastrConfig
+) {
+    'ngInject';
 
-  var config = {
-    apiKey: "AIzaSyDwKDlKS4wIesup_oJHlxqlV6XQFr3uMPU",
-    authDomain: "cision-feedback-dev.firebaseapp.com",
-    databaseURL: "https://cision-feedback-dev.firebaseio.com",
-    projectId: "cision-feedback-dev",
-    storageBucket: "cision-feedback-dev.appspot.com",
-    messagingSenderId: "984448149929"
-  };
-  firebase.initializeApp(config);
-  
-  // $httpProvider.interceptors.push(authInterceptor);
-  $stateProvider
-    .state('app', {
-      abstract: true,
-      templateUrl: 'layout/app-view.html'
+    var config = {
+        apiKey: 'AIzaSyDwKDlKS4wIesup_oJHlxqlV6XQFr3uMPU',
+        authDomain: 'cision-feedback-dev.firebaseapp.com',
+        databaseURL: 'https://cision-feedback-dev.firebaseio.com',
+        projectId: 'cision-feedback-dev',
+        storageBucket: 'cision-feedback-dev.appspot.com',
+        messagingSenderId: '984448149929'
+    };
+    firebase.initializeApp(config);
+
+    // $httpProvider.interceptors.push(authInterceptor);
+    $stateProvider.state('app', {
+        abstract: true,
+        templateUrl: 'layout/app-view.html'
     });
 
-  $urlRouterProvider.otherwise('/');
-  
-  angular.extend(toastrConfig, {
-    autoDismiss: false,
-    containerId: 'toast-container',
-    maxOpened: 0,    
-    newestOnTop: true,
-    positionClass: 'toast-top-center',
-    preventDuplicates: false,
-    preventOpenDuplicates: false,
-    target: 'body'
-  });
-
+    $urlRouterProvider.otherwise('/');
+    // $locationProvider.html5Mode({
+    //   enabled: true,
+    //   requireBase: true,
+    //   rewriteLinks: true
+    // });
+    angular.extend(toastrConfig, {
+        autoDismiss: false,
+        containerId: 'toast-container',
+        maxOpened: 0,
+        newestOnTop: true,
+        positionClass: 'toast-top-center',
+        preventDuplicates: false,
+        preventOpenDuplicates: false,
+        target: 'body'
+    });
 }
 
 export default AppConfig;
