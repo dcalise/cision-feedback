33ab57f0fc1d0554730ec465890a8f5b3821c5d9
Dan Calise <calise@gmail.com>
2018-09-26 08:45:14 -0400

    cleaning up deploy

--- src/app/config/app.run.js
+++ src/app/config/app.run.js
@@ -2,11 +2,11 @@ function AppRun(AppConstants, $rootScope, $state) {
     'ngInject';
 
     // change page title based on state
-    $rootScope.$on('$stateChangeSuccess', (toState) => {
+    $rootScope.$on('$stateChangeSuccess', (event, toState) => {
         $rootScope.setPageTitle(toState.title);
     });
 
-    $rootScope.$on("$stateChangeError", (error) => {
+    $rootScope.$on("$stateChangeError", (event, toState, toParams, fromState, fromParams, error) => {
         if (error === "AUTH_REQUIRED") {
             $state.go('app.login');
         }
