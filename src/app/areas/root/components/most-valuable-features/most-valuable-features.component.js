0790f9cd550180c0ffb31be689232596fd57a4bf
Dan Calise <calise@gmail.com>
2018-09-27 13:51:58 -0400

    real data for most valuable features

--- src/app/areas/root/components/most-valuable-features/most-valuable-features.component.js
+++ src/app/areas/root/components/most-valuable-features/most-valuable-features.component.js
@@ -1,6 +1,18 @@
 class MostValuableFeaturesCtrl {
-    constructor() {
+    constructor(FeatureService, $firebaseArray) {
         'ngInject';
+
+        this._FeatureService = FeatureService;
+        this.$firebaseArray = $firebaseArray;
+
+    }
+
+    $onInit() {
+        this.data = this.$firebaseArray(this._FeatureService._featuresRef.orderByChild('totalValue').limitToLast(8));
+
+        this.data.$loaded().then(
+            data => this.data = data.reverse()
+        );
     }
 }
 
