f86199eba0d3580a5b4827c8a6584d9165a7fd70
Dan Calise <calise@gmail.com>
2018-09-27 13:50:50 -0400

    accounts by type static data

--- src/app/areas/root/components/accounts-by-type/accounts-by-type.component.js
+++ src/app/areas/root/components/accounts-by-type/accounts-by-type.component.js
@@ -1,6 +1,53 @@
 class AccountsByTypeCtrl {
-    constructor() {
+    constructor(AppConstants) {
         'ngInject';
+
+        this.accountTypes = AppConstants.strings.account.accountTypes;
+
+        this.data = [21,1,4,62];
+    }
+
+    $onInit() {
+        const accountsByTypeElement = document.getElementById('accounts-by-type');
+
+        this.accountsByTypeChart = new Chart(accountsByTypeElement, {
+            type: 'bar',
+            data: {
+                labels: this.accountTypes,
+                datasets: [{
+                    data: this.data,
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 206, 86, 0.2)',
+                        'rgba(75, 192, 192, 0.2)',
+                        'rgba(153, 102, 255, 0.2)',
+                        'rgba(255, 159, 64, 0.2)'
+                    ],
+                    borderColor: [
+                        'rgba(255,99,132,1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                        'rgba(153, 102, 255, 1)',
+                        'rgba(255, 159, 64, 1)'
+                    ],
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero: true
+                        }
+                    }]
+                },
+                legend: false
+            }
+        });
+
+        this.$apply;
     }
 }
 
